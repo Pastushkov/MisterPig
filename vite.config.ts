c1d1175a2fda802a29f4b26667ad628fa42dae3f
Danylo Herhert <danylo.herhert@wisercat.net>
2024-01-12 17:42:35 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MisterPig/',
   server: {
     strictPort: true,
     port: 3000,
